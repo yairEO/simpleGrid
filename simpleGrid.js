38d444cf4ac340a5f762f22f63eafef5f650ae3a
vsync <vsync.design@gmail.com>
2014-06-17 15:35:57 +0300

    added an option to hide the last row if it doesn't have enough elements to fill it.
    Also, added a "destroy" method for cleanup
    
    extra minor adjustments.

--- simpleGrid.js
+++ simpleGrid.js
@@ -1,10 +1,14 @@
+// By Yair Even-Or
+// dropthebit.com
+
 ;(function ($) {
     "use strict";
 
     var defaults = {
         margin      : 1,   // in percentage, divides across al items
         initialSize : 150, // aim for this size (in PX)
-        minSize     : 100
+        minSize     : 100,
+        fullRows    : false // hide the last row if it doesn't have enough items to fill all of it
     };
 
     function createStylesheet(selector){
@@ -12,30 +16,35 @@
         return style.appendTo('head')[0];
     }
 
-    $.fn.simpleGrid = function (options){
+    $.fn.simpleGrid = function(options){
         defaults.selector = this.selector;
-        defaults.itemTagName = $(this.selector).find(':first-child')[0].tagName.toLowerCase();
 
+        defaults.itemTagName = options.itemTag || $(this).find(':first-child')[0].tagName.toLowerCase();
+
+        // instatiate a new 'Sticky' object per item that needs to be floated
         return this.each(function(){
             var $obj = $(this),
                 simpleGrid = $obj.data('_simpleGrid');
 
             options = options || {};
-
+            // if already bounded on the element
             if( simpleGrid ){
-                simpleGrid.calc();
+                if( !$.isEmptyObject(options) ){ // update options
+                    options = $.extend({}, simpleGrid.options, options);
+                    simpleGrid.options = options;
+                    simpleGrid.calc(true);
+                }
+                else
+                  simpleGrid.calc();
+
                 return this;
             }
 
             if( typeof options === 'object' )
                 options = $.extend({}, defaults, options);
 
-            else if( typeof options === 'string' )
-                return this;
-
             // instanciate a new SimpleGrid
-            simpleGrid = new SimpleGrid($obj[0], options);
-            simpleGrid.init();
+            simpleGrid = new SimpleGrid($obj, options);
 
             $obj.data('_simpleGrid', simpleGrid);
         });
@@ -43,23 +52,26 @@
 
     // Constructor
     function SimpleGrid(obj, options){
-        this.el          = obj;
-        this.rowItemsNum = 0;
+        this.id          = new Array(8).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, 7);
+        this.$el         = obj;
+        this.el          = obj[0];
+        this.itemsPerRow = 0;
         this.options     = options;
         this.selector    = this.options.selector + ' > '  + this.options.itemTagName;
         this.styleTag    = createStylesheet(this.selector);
 
-        this.init();
-    }
-
-    SimpleGrid.prototype.init = function(){
-        $(window).on('resize', this.calc.bind(this) );
-        $(this.el).on("webkitTransitionEnd transitionend oTransitionEnd", this.calc.bind(this) );
+        $(window).on('resize.simpleGrid' + this.id, this.calc.bind(this) );
         this.calc();
     }
 
-    SimpleGrid.prototype.calc = function(){
-        var itemsCount = this.el.children.length,
+    SimpleGrid.prototype.destroy = function(){
+        $(window).off('resize.simpleGrid' + this.id);
+        this.styleTag.remove();
+    }
+
+    SimpleGrid.prototype.calc = function(forceUpdate){
+        var items      = this.el.children,
+            itemsCount = items.length,
             rowWidth   = this.el.clientWidth,
             O          = this.options,
             itemSize   = 0,
@@ -67,44 +79,61 @@
 
         // THE MOST IMPORTANT THING:
         // Do not continue if number of items per row hasn't changed
-        // Or when there aren't enough items to fill a row:
-        if( this.rowItemsNum == ((rowWidth / O.initialSize)|0) )
+        if( !forceUpdate && this.itemsPerRow == ((rowWidth / O.initialSize)|0) )
             return;
 
-        this.rowItemsNum = (rowWidth / O.initialSize)|0;
+        this.itemsPerRow = (rowWidth / O.initialSize)|0;
 
-        if( this.rowItemsNum > itemsCount ){
-            this.updateRule();
-            return;
+        if( O.fullRows ){
+            this.$el.find('.hidden').removeClass('hidden'); // cleanup
+
+            var howManyTohide = itemsCount % this.itemsPerRow,
+                elmsToHide;
+
+            if( howManyTohide ){
+                elmsToHide = [].slice.call( items, -howManyTohide );
+                elmsToHide.forEach(function(item){
+                    $(item).addClass('hidden');
+                });
+            }
         }
 
-        
+        // when there aren't enough items to fill a row:
+        if( this.itemsPerRow > itemsCount ){
+            this.itemsPerRow = itemsCount;
+            // this.updateRule(); // remove the styling
+            // return;
+        }
 
-        itemSize = (100 - O.margin*(this.rowItemsNum-1)) / this.rowItemsNum; // in '%'
+        itemSize = (100 - O.margin*(this.itemsPerRow-1)) / this.itemsPerRow; // in '%'
 
         // check if new size is less than the minimum allowed
         // if so, show less item's per-row
         if( this.el.children[0].clientWidth < O.minSize ){
-            if( this.rowItemsNum > 1 )
-                this.rowItemsNum--;
+            if( this.itemsPerRow > 1 )
+                this.itemsPerRow--;
             else return;
         }
 
-        this.updateRule(itemSize);
+        this.updateRule(itemSize, O.margin);
     }
 
-    SimpleGrid.prototype.updateRule = function(width){
+    SimpleGrid.prototype.updateRule = function(width, margin){
         // the style sheet in the style tag
-        var sheet = this.styleTag.sheet ? this.styleTag.sheet : this.styleTag.styleSheet;
-        // the first rule in the style sheet
-        var rules = sheet.cssRules ? sheet.cssRules : sheet.rules,
-            delMethod = sheet.cssRules ? 'deleteRule' : 'removeRule',
-            addMethod = sheet.cssRules ? 'insertRule' : 'addRule';
+        var sheet = this.styleTag.sheet ? this.styleTag.sheet : this.styleTag.styleSheet,
+            rules = sheet.cssRules ? sheet.cssRules : sheet.rules,
+            cssText = 'width:' + width + '%; margin-right:'+ margin+ '%; margin-bottom:'+ margin+ '%';
 
-        rules[0].style.cssText = width ? 'width:' + width + '%;' : '';
+        rules[0].style.cssText = width ? cssText : '';
 
-        sheet[delMethod](1);
-        sheet[addMethod](this.selector + ':nth-child('+ this.rowItemsNum +'n){margin-right:0}', 1);
+        if( sheet.cssRules ){
+            sheet.deleteRule(1);
+            sheet.insertRule(this.selector + ':nth-child('+ this.itemsPerRow +'n){margin-right:0 !important}', 1);
+        }
+        else{
+            sheet.removeRule(1);
+            sheet.addRule(this.selector + ':nth-child('+ this.itemsPerRow +'n)', 'margin-right:0 !important', 1);
+        }
     }
 
 })(jQuery);
